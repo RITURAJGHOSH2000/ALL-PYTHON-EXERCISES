4e541f7ddd31d735672de15e8c2cc7eec9fa73d0
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-28 21:54:26 +0530

    Delete Widgets and Gizmos.py

--- The Python Workbook 2nd Ed. - Ben Stephenson.(Springer)/Widgets and Gizmos.py
+++
@@ -1,6 +0,0 @@
-widgets = int(input('Enter the number of widgets: '))
-gizmos = int(input('Enter the number of gizmos: '))
-weight_of_widgets = int(widgets * 75)
-weight_of_gizmos = int(gizmos * 112)
-print(
-    f'Total weight of widgets = {weight_of_widgets} grams\nTotal weight of gizmos = {weight_of_gizmos} grams')
