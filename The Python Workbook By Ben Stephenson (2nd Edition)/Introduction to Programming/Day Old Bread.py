b853393b93941de95996c0e9a8051fd38939249e
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-07-01 17:54:41 +0530

    Delete The Python Workbook By Ben Stephenson (2nd Edition)/Introduction to Programming directory

--- The Python Workbook By Ben Stephenson (2nd Edition)/Introduction to Programming/Day Old Bread.py
+++
@@ -1,7 +0,0 @@
-number_of_day_old_bread = int(input('Enter the number of day old breads: '))
-fresh_bread_price = 3.49
-day_old_bread_price = number_of_day_old_bread * fresh_bread_price * 0.60
-day_old_bread_price_format = "{:.2f}".format(day_old_bread_price)
-price_to_pay = fresh_bread_price - day_old_bread_price
-price_to_pay_format = "{:.2f}".format(price_to_pay)
-print(f'The regular price of the fresh bread is ${fresh_bread_price}\nAs the bread is a day old so it has a discount of 60%\nThe price which will be deducted due to discounht is {day_old_bread_price_format}\nThe price you have to pay is ${price_to_pay_format}')
--- The Python Workbook By Ben Stephenson (2nd Edition)/Introduction to Programming/Sort 3 integers.py
+++
@@ -1,3 +0,0 @@
-n = list(input('Enter the 3 numbers: '))
-n.sort()
-print('The sorted numbers from smallest to largest is: ', n)
